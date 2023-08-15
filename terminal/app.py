2cd4009817a4dd7d08f51644a0336cbb0fe2e4d3
giedriuzz <kraujinis@gmail.com>
2023-08-15 08:21:15 +0300

    work on teminal gui

--- terminal/app.py
+++ terminal/app.py
@@ -1,8 +1,9 @@
 """Main game script"""
 import time
+import sys
 from termcolor import colored, cprint
 from word import Word
-from validation import input_only_string
+from validation import input_only_string, input_only_integer_value_not_bigger
 from hangman import hangman
 
 word = Word("Hello")
@@ -11,48 +12,65 @@ greeting = colored(
     " === Welcome to Hangman game! ===", "white", "on_light_blue", attrs=["bold"]
 )
 print("\n", greeting, "\n")
-EMPTY_WORD = word.empty_word_list
-print("Guess the word: ", *EMPTY_WORD)
 
-LENGTH = 10
-while 0 < LENGTH:
-    LENGTH -= 1
-    not_guessed_letters = word.not_guessed_letters_list
-    letter = input_only_string(colored("Guess a letter or all word: ", "yellow"))
-    hanged = colored(hangman[7], "red", "on_light_blue", attrs=["blink"])
-    if word.is_letter_used(letter) is True:
-        if len(letter) == 1:
-            if word.is_letter_in_word(letter=letter) is True:
-                guessing_word = word.replace_guessed_letter(letter=letter)
-                joined_guessing_word = "".join(guessing_word)
-                if joined_guessing_word.isalpha() is True:
-                    print(
-                        f" = You guessed the word !!! -> {colored(joined_guessing_word,'yellow', attrs=['blink'] )}"
-                    )
+CHOOSING = int(
+    input_only_integer_value_not_bigger(
+        3, "1. Start the game\n2. Rules\n3. Quit\nChoose: "
+    )
+)
 
-                    break
+if CHOOSING == 1:
+    # Game area
+    EMPTY_WORD = word.empty_word_list
+    _SAY_DONT_GUESSED = "You don`t guessed a letter :("
+    _SAY_GUESSED_LETTER = "You guessed a letter ! "
+    print("Guess the word: ", *EMPTY_WORD)
 
-                print(*guessing_word)
-                print(f"You have {LENGTH} guesses left")
-            else:
-                letter_index = not_guessed_letters.index(letter.upper())
-                if letter_index <= 7:
-                    print("You dont guessed a letter")
+    LENGTH = 10
+    while 0 < LENGTH:
+        LENGTH -= 1
+        not_guessed_letters = word.not_guessed_letters_list
+        letter = input_only_string(colored("Guess a letter or all word: ", "yellow"))
+        hanged = colored(hangman[7], "red")
+        if word.is_letter_used(letter) is True:
+            if len(letter) == 1:
+                if word.is_letter_in_word(letter=letter) is True:
+                    guessing_word = word.replace_guessed_letter(letter=letter)
+                    JOINED_GUESSING_WORD = "".join(guessing_word)
+                    if JOINED_GUESSING_WORD.isalpha() is True:
+                        print(
+                            f" = You guessed the word !!! -> {colored(JOINED_GUESSING_WORD,'yellow', attrs=['blink'] )}"
+                        )
+                        break
+                    print(colored(_SAY_GUESSED_LETTER, "green"))
+                    print(*guessing_word)
                     print(f"You have {LENGTH} guesses left")
-                    print(hangman[letter_index])
                 else:
+                    letter_index = not_guessed_letters.index(letter.upper())
+                    if letter_index <= 7:
+                        print(colored(_SAY_DONT_GUESSED, "light_red"))
+                        print(f"You have {colored(LENGTH, 'red')} guesses left.")
+                        print(hangman[letter_index])
+                    else:
+                        print(hanged)
+                        break
+            else:
+                if word.is_guessed_word_equal(letter) is True:
+                    print("You guessed the word !!!")
+                    break
+                if word.is_guessed_word_equal(letter) is False:
                     print(hanged)
+                    print("You hanged !!!")
                     break
         else:
-            if word.is_guessed_word_equal(letter) is True:
-                print("You guessed the word !!!")
-                break
-            if word.is_guessed_word_equal(letter) is False:
-                print(hanged)
-                print("You hanged !!!")
-                break
-    else:
-        print(f"You used '{letter}' letter  before!")
-        print(f"You have {LENGTH} guesses left")
+            print(f"You used '{letter}' letter  before!")
+            print(f"You have {LENGTH} guesses left")
 
-    print("Used letter: ", *not_guessed_letters)
+        print("Used letter: ", *not_guessed_letters)
+elif CHOOSING == 2:
+    print("Rules area")
+
+
+elif CHOOSING == 3:
+    print("Quit area")
+    sys.exit(0)
--- terminal/validation.py
+++ terminal/validation.py
@@ -9,3 +9,14 @@ def input_only_string(input_text: str) -> str:
         if string.replace(" ", "").isalpha() is True:
             return string
         print("Input must to be a string !")
+
+
+def input_only_integer_value_not_bigger(value_size: int, input_text: str) -> str:
+    """Validate input is integer not bigger than value size
+    value_size: what value max size can be
+    input_text: what text want to see in input line"""
+    while True:
+        string = input(input_text)
+        if string.isdigit() is True and int(string) <= value_size:
+            return string
+        print(f"Input must to be an integer value not bigger than: {value_size} !")
